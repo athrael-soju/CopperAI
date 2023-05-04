700a020d3595552f9b18762b23346c037ffb18bd
Athrael Soju <athrael.soju@gmail.com>
2023-05-04 22:54:09 +0100

    First draft of Redis Cache implementation.

--- pinecone/src/routes/query.js
+++ pinecone/src/routes/query.js
@@ -4,26 +4,37 @@ import { getIndex } from "../utils/utils.js";
 
 const router = express.Router();
 
-const queryRoute = (pinecone) => {
+const queryRoute = (pinecone, redisClient) => {
   router.post("/", async (req, res) => {
-    let index = await getIndex(pinecone);
     const { message, topK } = req.body;
-    console.log("Querying message:", message);
 
-    let vector = await createEmbedding(message);
-    console.log("Embedded message:", vector);
+    let redisResponse = await redisClient.get(message);
+    if (redisResponse) {
+      redisResponse = JSON.parse(redisResponse);
+      console.log(
+        "Redis: cache hit. Returning Redis Response",
+        redisResponse.matches[0]
+      );
+      return res.status(200).json(redisResponse);
+    } else {
+      let index = await getIndex(pinecone);
+      let vector = await createEmbedding(message);
 
-    const queryResponse = await index.query({
-      queryRequest: {
-        namespace: process.env.PINECONE_NAMESPACE,
-        topK: topK,
-        includeValues: true,
-        includeMetadata: true,
-        vector: vector,
-      },
-    });
-    console.log("Query response:", queryResponse);
-    res.status(200).json(queryResponse);
+      console.log("Pinecone: querying message:", message);
+      const queryResponse = await index.query({
+        queryRequest: {
+          namespace: process.env.PINECONE_NAMESPACE,
+          topK: topK,
+          includeValues: true,
+          includeMetadata: true,
+          vector: vector,
+        },
+      });
+      console.log("Pinecone: query response:", queryResponse.matches[0]);
+      await redisClient.set(message, JSON.stringify(queryResponse));
+      console.log("Redis: response cached: ", queryResponse.matches[0]);
+      res.status(200).json(queryResponse);
+    }
   });
   return router;
 };
--- pinecone/src/server.js
+++ pinecone/src/server.js
@@ -4,27 +4,36 @@ import { PineconeClient } from "@pinecone-database/pinecone";
 import dotenv from "dotenv";
 import queryRoute from "./routes/query.js";
 import upsertRoute from "./routes/upsert.js";
-
+import { createClient } from "redis";
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PINECONE_PORT;
+let pinecone, redisClient;
 
 app.listen(port, async () => {
   try {
-    let pinecone = new PineconeClient();
+    pinecone = new PineconeClient();
     await pinecone.init({
       environment: process.env.PINECONE_ENVIRONMENT,
       apiKey: process.env.PINECONE_API_KEY,
     });
 
-    app.use("/query", queryRoute(pinecone));
+    redisClient = createClient({ url: process.env.REDIS_URI });
+    redisClient.on("error", (err) => console.log("Redis Client Error", err));
+    await redisClient.connect();
+    console.log("Connected to Redis");
+
+    app.use("/query", queryRoute(pinecone, redisClient));
     app.use("/upsert", upsertRoute(pinecone));
 
     console.log(`Pinecone service listening at http://localhost:${port}`);
   } catch (err) {
     console.log("Error connecting to Pinecone", err);
+    redisClient.disconnect();
+    process.exit();
   }
 });
