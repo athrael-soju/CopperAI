4b240d7c86a8eba1318c57f491219a88b5a71241
Athrael Soju <athrael.soju@gmail.com>
2023-05-05 00:00:38 +0100

    Set up Unit Test libraries.

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,20 +19,24 @@ app.use("/message", messageRoute);
 app.use("/speak", speakRoute);
 app.use("/auth", authRoute);
 
-app.listen(process.env.SERVER_PORT, async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-    if (process.env.PINECONE_ENABLED === "true") {
-      console.log("Pinecone access enabled");
-    } else {
-      console.log("Pinecone access disabled");
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(process.env.SERVER_PORT, async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+      if (process.env.PINECONE_ENABLED === "true") {
+        console.log("Pinecone access enabled");
+      } else {
+        console.log("Pinecone access disabled");
+      }
+    } catch (err) {
+      console.error("Error connecting to mongodb", err);
     }
-  } catch (err) {
-    console.error("Error connecting to mongodb", err);
-  }
-  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
-});
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+  });
+}
+export default app;
