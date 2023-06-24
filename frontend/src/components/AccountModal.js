5ba66e77756d0d9a8734c297a8ec5451554f2d49
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2023-06-24 16:04:27 +0300

    Small improvement.

--- frontend/src/components/AccountModal.js
+++ frontend/src/components/AccountModal.js
@@ -3,11 +3,14 @@ import { Modal } from "antd";
 
 const AccountModal = ({ show, handleClose, children }) => {
   return (
-    <Modal show={show} onHide={handleClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Account</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>{children}</Modal.Body>
+    <Modal
+      title="Account"
+      okText="Login"
+      open={show}
+      onCancel={handleClose}
+      closable
+    >
+      {children}
     </Modal>
   );
 };
