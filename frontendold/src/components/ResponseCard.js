2cfdf764ed0a9b10d087858d44498ce85e30625f
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-07-05 18:10:36 +0300

    WIP

--- frontendold/src/components/ResponseCard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ChatResponse from "./ChatResponse";
-
-const ResponseCard = ({ response }) => {
-  return <ChatResponse response={response} />;
-};
-
-export default React.memo(ResponseCard);
