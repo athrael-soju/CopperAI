4d302b5fd4e989af9039fed9efd87cd05d7bf571
Athrael Soju <athrael.soju@gmail.com>
2023-07-06 17:12:01 +0100

    Add new frontend skeleton

---
+++ next-frontend/components/Navbar.tsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between p-6 bg-blue-500 text-white">
+      <a href="/" className="text-lg font-bold">
+        Logo
+      </a>
+      <div>
+        <a href="/" className="p-2">
+          Home
+        </a>
+        <a href="/about" className="p-2">
+          About
+        </a>
+        <a href="/contact" className="p-2">
+          Contact
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
