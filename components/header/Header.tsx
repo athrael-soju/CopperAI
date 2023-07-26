aa9d61f81abd4826e4ce07cdfa27e8b896c5ffa1
Athrael Soju <athrael.soju@gmail.com>
2023-07-26 16:37:15 +0100

    Refactoring & file upload button

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useContext, useRef } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSignInAlt,
+  faSignOutAlt,
+  faUpload,
+} from '@fortawesome/free-solid-svg-icons';
+
 import ChatTypeContext from '../../lib/Context/ChatType'; // update the import path as needed
 
 export default function Header() {
+  const fileInputRef = useRef<HTMLInputElement>(null); // specify the type here
   const { data: session, status } = useSession();
   const chatTypeContext = useContext(ChatTypeContext);
 
@@ -13,72 +21,97 @@ export default function Header() {
 
   const { chatType } = chatTypeContext;
 
-  const handleFileUpload = (event: { target: { files: any[] } }) => {
-    const file = event.target.files[0];
-    console.log(file); // Handle the file as needed
+  const handleButtonClick = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    // check if the ref is attached to a DOM element before calling click
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      console.log('File Uploaded: ', file); // Handle the file as needed
+    }
   };
 
   return (
     <header className="fixed top-0 left-0 right-0 text-center text-white bg-copper-200 h-16 flex items-center justify-center px-20 shadow-md">
       <div className="flex justify-between w-full items-center">
         <nav>
-          <ul className="flex gap-5 list-none m-0">
-            <li>
+          <ul className="flex gap-5 list-none m-0 items-center h-full">
+            <li className="mr-auto">
               <a
-                className="text-white underline hover:text-gray-300"
+                className="text-shadow-default text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-palette2564_1 via-palette2564_3 to-palette2564_5"
                 href="https://github.com/athrael-soju/whisperChat"
                 target="_blank"
                 rel="noreferrer"
               >
-                whisperChat
+                Copper AI
               </a>
-            </li>
-            <li>
-              {chatType === 'documentChat' && (
-                <button
-                  className="text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 transform hover:scale-105 hover:bg-palette419_3 border border-palette419_5"
-                  onClick={handleFileUpload}
-                >
-                  Upload File
-                </button>
-              )}
             </li>
           </ul>
         </nav>
         {!session && (
           <div className="flex gap-5 items-center">
-            <span>You are not signed in</span>
             <button
               className="text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 transform hover:scale-105 hover:bg-palette419_3 border border-palette419_5"
               onClick={(e) => {
                 e.preventDefault();
                 signIn();
               }}
+              title="Sign in"
             >
-              Sign in
+              <FontAwesomeIcon icon={faSignInAlt} />
             </button>
+            <span
+              className="w-10 h-10 bg-cover rounded-full"
+              style={{ backgroundColor: 'lightgray' }}
+              title="Signed Out"
+            />
           </div>
         )}
         {session?.user && (
           <div className="flex items-center gap-2">
-            {session.user.image && (
-              <span
-                className="w-10 h-10 bg-cover rounded-full"
-                style={{ backgroundImage: `url('${session.user.image}')` }}
-              />
+            {chatType === 'documentChat' && (
+              <>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleFileUpload}
+                  style={{ display: 'none' }} // Hide the input element
+                />
+                <button
+                  className="text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 transform hover:scale-105 hover:bg-palette419_3 border border-palette419_5"
+                  onClick={handleButtonClick} // use handleButtonClick here
+                  title="Upload File"
+                >
+                  <FontAwesomeIcon icon={faUpload} />
+                </button>
+              </>
             )}
-            <span className="mr-2 text-gray-800">
-              <strong>{session.user.email ?? session.user.name}</strong>
-            </span>
             <button
               className="text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 transform hover:scale-105 hover:bg-palette419_3 border border-palette419_5"
               onClick={(e) => {
                 e.preventDefault();
                 signOut();
               }}
+              title="Sign out"
             >
-              Sign out
+              <FontAwesomeIcon icon={faSignOutAlt} />
             </button>
+            <span
+              className="w-10 h-10 bg-cover rounded-full"
+              style={{
+                backgroundImage: session.user.image
+                  ? `url('${session.user.image}')`
+                  : 'lightgray',
+              }}
+              title={
+                session.user.email ?? session.user.name ?? 'No name provided'
+              }
+            />
           </div>
         )}
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import { useSession } from 'next-auth/react';
 import Recorder from '../components/Recorder';
 import { useContext } from 'react';
 import ChatTypeContext from '../lib/Context/ChatType'; // update the import path as needed
-
 export default function Home() {
   const router = useRouter();
   const { data: session, status } = useSession({
@@ -25,18 +24,18 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center p-24">
+    <main className="flex flex-col min-h-screen justify-center items-center p-24">
       {!chatType && session && (
-        <div className="w-full space-y-8">
+        <div className="space-y-8 w-full">
           <button
-            className="w-full py-4 px-8 text-white rounded font-bold text-2xl"
+            className="w-full py-4 px-8 text-white rounded font-bold text-2xl transition-transform duration-200 transform hover:scale-105"
             style={{ backgroundColor: '#b7b09b' }}
             onClick={() => chooseChatType('generalChat')}
           >
             General Chat
           </button>
           <button
-            className="w-full py-4 px-8 text-white rounded font-bold text-2xl"
+            className="w-full py-4 px-8 text-white rounded font-bold text-2xl transition-transform duration-200 transform hover:scale-105"
             style={{ backgroundColor: '#e1bc9a' }}
             onClick={() => chooseChatType('documentChat')}
           >
@@ -44,7 +43,6 @@ export default function Home() {
           </button>
         </div>
       )}
-      <div className="flex-grow" />
       {chatType && session && <Recorder className="mb-10" />}
     </main>
   );
