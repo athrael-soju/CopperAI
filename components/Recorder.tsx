c21175e838268dcecc698b83b430f1949163fe0b
Athrael Soju <athrael.soju@gmail.com>
2023-08-14 21:03:42 +0100

    commit changes

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,13 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from 'react-speech-recognition';
 import { RecordButton, StopButton } from './Buttons';
-import useProcessRecording from '../hooks/useProcessRecording';
-import { useSession } from 'next-auth/react';
-import useAudioSensitivity from '../hooks/useAudioSensitivity';
-
+import { useRecorder } from '../hooks/useRecorder'; // Make sure to correctly import the path
+import SpeechRecognition from 'react-speech-recognition';
 const appId: string = process.env.NEXT_PUBLIC_SPEECHLY_APP_ID || '';
 const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
 SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
@@ -25,95 +20,12 @@ const Recorder: React.FC<RecorderProps> = ({
   namespace,
   handleAudioElement,
 }) => {
-  const isMicActive = useAudioSensitivity();
-  const { data: session } = useSession();
-  const { transcript, browserSupportsSpeechRecognition, resetTranscript } =
-    useSpeechRecognition();
-
-  const [isListening, setIsListening] = useState(false);
-  const [newTranscript, setNewTranscript] = useState<string | null>(null);
   const {
-    status,
-    setStatus,
-    setRecordingProcessed,
-    stopOngoingAudio,
-    audioRef,
-  } = useProcessRecording(newTranscript, session, setIsLoading, namespace);
-  const timeoutRef = useRef<number | null>(null);
-  const [buttonState, setButtonState] = useState<'record' | 'stop' | null>(
-    null
-  );
-
-  useEffect(() => {
-    console.log('status', status);
-  }, [isMicActive, status]);
-
-  useEffect(() => {
-    if (buttonState === 'record') {
-      // If the mic is active (user is speaking)
-      if (isMicActive) {
-        SpeechRecognition.startListening({ continuous: true });
-        // Clear any existing timeout (if present)
-        if (timeoutRef.current) {
-          clearTimeout(timeoutRef.current);
-          timeoutRef.current = null;
-        }
-      } else if (transcript?.length > 0) {
-        // If mic is not active, start a timeout of 2 seconds
-        timeoutRef.current = window.setTimeout(() => {
-          SpeechRecognition.stopListening();
-          console.log('transcript', transcript);
-          setNewTranscript(transcript);
-          setRecordingProcessed(false);
-          resetTranscript();
-          setStatus('idle');
-        }, 3000); // 3 second grace period
-      }
-      // Clear the timeout when the component is unmounted
-      return () => {
-        if (timeoutRef.current) {
-          clearTimeout(timeoutRef.current);
-        }
-      };
-    }
-  }, [
+    stopButtonEvent,
     buttonState,
-    isMicActive,
-    resetTranscript,
-    setRecordingProcessed,
-    setStatus,
-    transcript,
-  ]);
-
-  useEffect(() => {
-    if (status === 'generated') {
-      handleAudioElement(audioRef.current);
-      setStatus('idle');
-    }
-  }, [status, setStatus, handleAudioElement, audioRef]);
-
-  const toggleListening = () => {
-    if (isListening) {
-      SpeechRecognition.stopListening();
-    } else {
-      SpeechRecognition.startListening({ continuous: true });
-    }
-    setIsListening(!isListening);
-  };
-
-  const stopButtonEvent = () => {
-    stopOngoingAudio();
-    toggleListening();
-    setStatus('idle');
-    setButtonState('stop');
-  };
-
-  const recordButtonEvent = () => {
-    toggleListening();
-    stopOngoingAudio();
-    setStatus('recording');
-    setButtonState('record');
-  };
+    recordButtonEvent,
+    browserSupportsSpeechRecognition,
+  } = useRecorder(appId, setIsLoading, namespace, handleAudioElement);
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn&apos;t support speech recognition.</span>;
@@ -121,7 +33,7 @@ const Recorder: React.FC<RecorderProps> = ({
 
   return (
     <div className={className}>
-      {isListening ? (
+      {buttonState === 'record' ? (
         <StopButton onClick={stopButtonEvent} />
       ) : (
         <RecordButton onClick={recordButtonEvent} />
